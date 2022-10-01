0602b887c5a0405b8a78cca0724083340402a4f0
milon <milonsheikh88@gmail.com>
2022-10-01 13:02:45 +0600

    Core Java Practices-String vs StringBuilder.

---
+++ src/string/StringStringBuilder.java
@@ -0,0 +1,17 @@
+package string;
+
+public class StringStringBuilder {
+    public static void main(String[] args) {
+        String string = "Hello";
+        string.concat(" World");
+        System.out.println("string====>>> " + string);
+
+        /** object is not modified.*/
+        string = string.concat(" World");
+        System.out.println("string====>>> " + string);
+
+        StringBuilder stringBuilder = new StringBuilder("Hello ");
+        stringBuilder.append("World");
+        System.out.println("stringBuilder====>>> " + stringBuilder);
+    }
+}
